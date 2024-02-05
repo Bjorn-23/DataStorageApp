c6f11d8345e9a991b8801a9c68e6b282977f87ba
Bjorn-23 <bjornandersson84@gmail.com>
2024-02-05 15:55:20 +0100

    Started servicetests

---
+++ Infrastructure.Tests/Repository_Tests/CategoryRepository_Tests.cs
@@ -0,0 +1,297 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Infrastructure.Interfaces;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Tests.Repository_Tests;
+
+public class CategoryRepository_Tests
+{
+
+    private readonly CategoryRepository _categoryRepository;
+
+    public CategoryRepository_Tests()
+    {
+        var context = new ProductCatalog(new DbContextOptionsBuilder<ProductCatalog>()
+        .UseInMemoryDatabase($"{Guid.NewGuid()}")
+        .Options);
+
+        _categoryRepository = new CategoryRepository(context);
+    }
+
+    [Fact]
+    public void Create_ShouldCreateNewEntityInDatabase_AndReturnIt()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+
+        //Act
+        var createResult = _categoryRepository.Create(category);
+
+        //Assert
+        Assert.NotNull(createResult);
+        Assert.Equal("Demo", createResult.CategoryName);
+        Assert.Equal(1, createResult.Id);
+    }
+
+
+    [Fact]
+    public void Create_ShouldNotCreateNewEntityInDatabase_AndReturnNull()
+    {
+        //Arrange
+        var category = new CategoryEntity() { };
+
+        //Act
+        var createResult = _categoryRepository.Create(category);
+
+        //Assert
+        Assert.Null(createResult);
+    }
+
+    [Fact]
+    public void GetAllShould_IfAnyEntityExists_ReturnAllEntitiesFromDataBase()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        var createResult = _categoryRepository.Create(category);
+
+        //Act
+        var getResult = _categoryRepository.GetAll();
+
+        //Assert
+        Assert.NotNull(getResult);
+        Assert.Equal("Demo", getResult.FirstOrDefault()!.CategoryName);
+    }
+
+    [Fact]
+    public void GetAll_ShouldReturnEmptyList_SinceNoEntitiesExistInDatabase()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        //var createResult = _categoryRepository.Create(address);
+
+        //Act
+        var getResult = _categoryRepository.GetAll();
+
+        //Assert
+        Assert.Empty(getResult);
+        Assert.NotNull(getResult);
+    }
+
+    [Fact]
+    public void GetAllWithPredicate_ShouldReturnAnyUser_MatchingLambdaExpressionFromDataBase()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        var createResult = _categoryRepository.Create(category);
+
+        //Act
+        var getResult = _categoryRepository.GetAllWithPredicate(x => x.CategoryName == createResult.CategoryName);
+
+        //Assert
+        Assert.NotNull(getResult);
+        Assert.Equal("Demo", getResult.FirstOrDefault()!.CategoryName);
+    }
+
+    [Fact]
+    public void GetAllWithPredicate_ShouldReturnEmptyList_SinceTheEntityInPredicate_DoesNotExistInDatabase()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        var createResult = _categoryRepository.Create(category);
+
+        CategoryEntity otherCategory = new() { CategoryName = "Test" };
+
+        //Act
+        var getResult = _categoryRepository.GetAllWithPredicate(x => x.CategoryName == otherCategory.CategoryName);
+
+        //Assert
+        Assert.NotNull(getResult);
+        Assert.Empty(getResult);
+    }
+
+    [Fact]
+    public void GetOne_ShouldIfEntityExists_ReturnOneEntityFromDataBase()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        var createResult = _categoryRepository.Create(category);
+
+        //Act
+        var getResult = _categoryRepository.GetOne(x => x.CategoryName == createResult.CategoryName);
+
+        //Assert
+        Assert.NotNull(getResult);
+        Assert.Equal("Demo", getResult.CategoryName);
+        Assert.Equal(1, getResult.Id);
+    }
+
+
+    [Fact]
+    public void GetOne_ShouldReturnNull_SinceNoEntitiesExists()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        //var createResult = _categoryRepository.Create(address);
+
+        //Act
+        var getResult = _categoryRepository.GetOne(x => x.CategoryName == category.CategoryName);
+
+        //Assert
+        Assert.Null(getResult);
+    }
+
+    [Fact]
+    public void Update_ShouldUpdateExistingEntity_ReturnUpdatedEntity_FromDataBase()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        var createResult = _categoryRepository.Create(category);
+
+        CategoryEntity updatedCategory = new() { Id = createResult.Id, CategoryName = "Test" };
+
+        //Act
+        var updatedResult = _categoryRepository.Update(createResult, updatedCategory);
+
+        //Assert
+        Assert.NotNull(updatedResult);
+        Assert.Equal("Test", updatedResult.CategoryName);
+        Assert.Equal(createResult.Id, updatedResult.Id);
+    }
+
+    [Fact]
+    public void Update_ShouldFailToUpdateExistingEntity_AndReturnNull()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        var createResult = _categoryRepository.Create(category);
+
+        CategoryEntity updatedCategory = new() { Id = 0, CategoryName = "Test" }; //changes Id which should make it fail.
+
+        //Act
+        var updatedResult = _categoryRepository.Update(createResult, updatedCategory);
+
+        //Assert
+        Assert.Null(updatedResult);
+    }
+
+    [Fact]
+    public void UpdateWithPredicate_ShouldUpdateExistingEntity_AndReturnUpdatedEntityFromDataBase()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        var createResult = _categoryRepository.Create(category);
+
+        CategoryEntity updatedCategory = new() { Id = createResult.Id, CategoryName = "Test" };
+
+        //Act
+        var updatedResult = _categoryRepository.Update(x => x.Id == createResult.Id, updatedCategory);
+
+        //Assert
+        Assert.NotNull(updatedResult);
+        Assert.Equal("Test", updatedResult.CategoryName);
+        Assert.Equal(createResult.Id, updatedResult.Id);
+    }
+
+    [Fact]
+    public void UpdateWithPredicate_ShouldNotUpdateExistingEntity_AndThenReturnNull()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        var createResult = _categoryRepository.Create(category);
+
+        CategoryEntity updatedCategory = new() { Id = 0, CategoryName = "Test" }; //changes Id which should make it fail.
+
+        //Act
+        var updatedResult = _categoryRepository.Update(x => x.Id == createResult.Id, updatedCategory);
+
+        //Assert
+        Assert.Null(updatedResult);
+    }
+
+    [Fact]
+    public void DeleteWithPredicate_ShouldDeleteExistingEntity_AndReturnTrue()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        var createResult = _categoryRepository.Create(category);
+
+        //Act
+        var updatedResult = _categoryRepository.Delete(x => x.Id == createResult.Id);
+
+        //Assert
+        Assert.True(updatedResult);
+    }
+
+    [Fact]
+    public void DeleteWithPredicate_ShouldNotDeleteAnyEntities_AndReturnFalse()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        //var createResult = _categoryRepository.Create(category);
+
+        //Act
+        var updatedResult = _categoryRepository.Delete(x => x.Id == category.Id);
+
+        //Assert
+        Assert.False(updatedResult);
+    }
+
+    [Fact]
+    public void Delete_ShouldDeleteExistingEntity_AndReturnTrue()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        var createResult = _categoryRepository.Create(category);
+
+        //Act
+        var updatedResult = _categoryRepository.Delete(createResult);
+
+        //Assert
+        Assert.True(updatedResult);
+    }
+
+    [Fact]
+    public void Delete_ShouldNotDeleteAnyEntities_AndReturnFalse()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        //var createResult = _categoryRepository.Create(category);
+
+        //Act
+        var updatedResult = _categoryRepository.Delete(category);
+
+        //Assert
+        Assert.False(updatedResult);
+    }
+
+    [Fact]
+    public void Exists_ShouldCheckForExistingEntity_AndReturnTrue()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        var createResult = _categoryRepository.Create(category);
+
+        //Act
+        var updatedResult = _categoryRepository.Exists(x => x.Id == createResult.Id);
+
+        //Assert
+        Assert.True(updatedResult);
+    }
+
+    [Fact]
+    public void Exists_ShouldCheckForExistingEntity_AndReturnFalse()
+    {
+        //Arrange
+        var category = new CategoryEntity() { CategoryName = "Demo" };
+        //var createResult = _categoryRepository.Create(category);
+
+
+        //Act
+        var updatedResult = _categoryRepository.Exists(x => x.Id == category.Id);
+
+        //Assert
+        Assert.False(updatedResult);
+    }
+}
---
+++ Infrastructure.Tests/Repository_Tests/CustomerRepository_Tests.cs
@@ -0,0 +1,308 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Infrastructure.Interfaces;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.Net;
+
+namespace Infrastructure.Tests.Repository_Tests;
+
+public class CustomerRepository_Tests
+{
+    private readonly CustomerRepository _customerRepository;
+    private readonly UserRepository _userRepository;
+    private readonly Customer_AddressRepository _customerAddressRepository;
+    private readonly UserRoleRepository _userRoleRepository;
+    private readonly AddressRepository _addressRepository;
+
+    public CustomerRepository_Tests()
+    {
+        var context = new DataContext(new DbContextOptionsBuilder<DataContext>()
+        .UseInMemoryDatabase($"{Guid.NewGuid()}")
+        .Options);
+
+        _customerRepository = new CustomerRepository(context);
+        _userRepository = new UserRepository(context);
+        _customerAddressRepository = new Customer_AddressRepository(context);
+        _userRoleRepository = new UserRoleRepository(context);
+        _addressRepository = new AddressRepository(context);
+    }
+
+    [Fact]
+    public void Create_ShouldCreateNewEntityInDatabase_AndReturnIt()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+
+        //Act
+        var createResult = _customerRepository.Create(customer);
+
+        //Assert
+        Assert.NotNull(createResult);
+        Assert.Equal("bjorn@domain.com", createResult.EmailId);
+    }
+
+    [Fact]
+    public void Create_ShouldNotCreateNewEntityInDatabase_AndReturnNull()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" }; // Email left out: EmailId = "bjorn@domain.com",
+
+        //Act
+        var createResult = _customerRepository.Create(customer);
+
+        //Assert
+        Assert.Null(createResult);
+    }
+
+    [Fact]
+    public void GetAllShould_IfAnyEntityExists_ReturnAllEntitiesFromDataBase()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+        var createResult = _customerRepository.Create(customer);
+
+        //Act
+        var getResult = _customerRepository.GetAll();
+
+        //Assert
+        Assert.NotNull(getResult);
+        Assert.Equal("bjorn@domain.com", getResult.FirstOrDefault()!.EmailId);
+    }
+
+    [Fact]
+    public void GetAll_ShouldReturnEmptyList_SinceNoEntitiesExistInDatabase()
+    {
+        //Arrange
+
+
+        //Act
+        var getResult = _customerRepository.GetAll();
+
+        //Assert
+        Assert.Empty(getResult);
+        Assert.NotNull(getResult);
+    }
+
+    [Fact]
+    public void GetAllWithPredicate_ShouldReturnAnyUser_MatchingLambdaExpressionFromDataBase()
+    {
+        //Arrange
+        var UserRole = _userRoleRepository.Create(new UserRoleEntity() { RoleName = "Demo Role" });
+        var user = _userRepository.Create(new UserEntity() { Email = "bjorn@domain.com", Password = "Bytmig123!", SecurityKey = "blabla", IsActive = true, UserRoleId = UserRole.Id });
+        var customer = new CustomerEntity() { EmailId = user.Email, FirstName = "Björn", LastName = "Andersson", Id = user.Id, PhoneNumber = "0789456123" };
+        var address = _addressRepository.Create(new AddressEntity() { StreetName = "Storgatan 1", PostalCode = "222 22", City = "Storstan", Country = "Sverige" });
+        var customer_address = _customerAddressRepository.Create(new Customer_AddressEntity() { AddressId = address.Id, CustomerId = customer.Id });
+        var createResult = _customerRepository.Create(customer);
+
+        //Act
+        var getResult = _customerRepository.GetAllWithPredicate(x => x.FirstName == createResult.FirstName);
+
+        //Assert
+        Assert.NotNull(getResult);
+        //Assert.Equal("Björn", getResult.FirstOrDefault()!.FirstName);
+    }
+
+    [Fact]
+    public void GetAllWithPredicate_ShouldReturnEmptyList_SinceTheEntityInPredicate_DoesNotExistInDatabase()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+        string otherRole = "Arne";
+        var createResult = _customerRepository.Create(customer);
+
+        //Act
+        var getResult = _customerRepository.GetAllWithPredicate(x => x.FirstName == otherRole);
+
+        //Assert
+        Assert.NotNull(getResult);
+        Assert.Empty(getResult);
+    }
+
+    [Fact]
+    public void GetOne_ShouldIfEntityExists_ReturnOneEntityFromDataBase()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+        var createResult = _customerRepository.Create(customer);
+
+        //Act
+        var getResult = _customerRepository.GetOne(x => x.EmailId == createResult.EmailId);
+
+        //Assert
+        Assert.NotNull(getResult);
+        Assert.Equal("bjorn@domain.com", getResult.EmailId);
+    }
+
+    [Fact]
+    public void GetOne_ShouldReturnNull_SinceNoEntitiesExists()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+        //var createResult = userRepository.Create(user); // User is never created in this test
+
+        //Act
+        var getResult = _customerRepository.GetOne(x => x.EmailId == customer.EmailId);
+
+        //Assert
+        Assert.Null(getResult);
+    }
+
+    [Fact]
+    public void Update_ShouldUpdateExistingEntity_ReturnUpdatedEntity_FromDataBase()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+        var createResult = _customerRepository.Create(customer);
+
+        var updatedCustomer = new CustomerEntity() { Id = customer.Id, FirstName = customer.FirstName, LastName = customer.LastName, PhoneNumber = "01253456789", EmailId = "bjorn@email.com" }; // Different email
+
+        //Act
+        var updatedResult = _customerRepository.Update(createResult, updatedCustomer);
+
+        //Assert
+        Assert.NotNull(updatedResult);
+        Assert.Equal("bjorn@email.com", updatedResult.EmailId);
+        Assert.NotEqual("0789456123", updatedResult.PhoneNumber);
+    }
+
+    [Fact]
+    public void Update_ShouldFailToUpdateExistingEntity_AndReturnNull()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+        var createResult = _customerRepository.Create(customer);
+
+        var updatedCustomer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" }; // No Id supplied
+
+        //Act
+        var updatedResult = _customerRepository.Update(createResult, updatedCustomer);
+
+        //Assert
+        Assert.Null(updatedResult);
+    }
+
+    [Fact]
+    public void UpdateWithPredicate_ShouldUpdateExistingEntity_AndReturnUpdatedEntityFromDataBase()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+        var createResult = _customerRepository.Create(customer);
+
+        var updatedCustomer = new CustomerEntity() { Id = customer.Id, FirstName = customer.FirstName, LastName = customer.LastName, PhoneNumber = "01253456789", EmailId = "bjorn@email.com" };
+        //Act
+        var updatedResult = _customerRepository.Update(x => x.Id == createResult.Id, updatedCustomer);
+
+        //Assert
+        Assert.NotNull(updatedResult);
+        Assert.Equal("bjorn@email.com", updatedResult.EmailId);
+        Assert.NotEqual("0789456123", updatedResult.PhoneNumber);
+    }
+
+    [Fact]
+    public void UpdateWithPredicate_ShouldNotUpdateExistingEntity_AndThenReturnNull()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+        var createResult = _customerRepository.Create(customer);
+
+        var updatedCustomer = new CustomerEntity() { EmailId = "bjorn@email.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "01253456789" };
+
+        //Act
+        var updatedResult = _customerRepository.Update(x => x.Id == createResult.Id, updatedCustomer);
+
+
+        //Assert
+        Assert.Null(updatedResult);
+    }
+
+    [Fact]
+    public void DeleteWithPredicate_ShouldDeleteExistingEntity_AndReturnTrue()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+        var createResult = _customerRepository.Create(customer);
+
+        //Act
+        var updatedResult = _customerRepository.Delete(x => x.Id == createResult.Id);
+
+        //Assert
+        Assert.True(updatedResult);
+    }
+
+    [Fact]
+    public void DeleteWithPredicate_ShouldNotDeleteAnyEntities_AndReturnFalse()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+        //var createResult = _customerRepository.Create(customer);
+
+        //Act
+        var updatedResult = _customerRepository.Delete(x => x.Id == customer.Id);
+
+        //Assert
+        Assert.False(updatedResult);
+    }
+
+    [Fact]
+    public void Delete_ShouldDeleteExistingEntity_AndReturnTrue()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+        var createResult = _customerRepository.Create(customer);
+
+
+        //Act
+        var updatedResult = _customerRepository.Delete(createResult);
+
+        //Assert
+        Assert.True(updatedResult);
+    }
+
+    [Fact]
+    public void Delete_ShouldNotDeleteAnyEntities_AndReturnFalse()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+        //var createResult = _customerRepository.Create(customer);
+
+
+        //Act
+        var updatedResult = _customerRepository.Delete(customer);
+
+        //Assert
+        Assert.False(updatedResult);
+    }
+
+    [Fact]
+    public void Exists_ShouldCheckForExistingEntity_AndReturnTrue()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+        var createResult = _customerRepository.Create(customer);
+
+
+        //Act
+        var updatedResult = _customerRepository.Exists(x => x.Id == createResult.Id);
+
+        //Assert
+        Assert.True(updatedResult);
+    }
+
+    [Fact]
+    public void Exists_ShouldCheckForExistingEntity_AndReturnFalse()
+    {
+        //Arrange
+        var customer = new CustomerEntity() { EmailId = "bjorn@domain.com", FirstName = "Björn", LastName = "Andersson", Id = Guid.NewGuid().ToString(), PhoneNumber = "0789456123" };
+        //var createResult = _customerRepository.Create(customer);
+
+
+        //Act
+        var updatedResult = _customerRepository.Exists(x => x.Id == customer.Id);
+
+        //Assert
+        Assert.False(updatedResult);
+    }
+}
+
